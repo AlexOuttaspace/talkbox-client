f034af2508328f8749de3c0faa0dd0801cecd0a0
alexander.pomazan <alexander.pomazan@jetruby.com>
2018-12-01 22:02:16 +0300

    feat: establish websocket connection with the server

--- src/common/init-apollo.js
+++ src/common/init-apollo.js
@@ -1,7 +1,39 @@
 import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost'
+import { split } from 'apollo-link'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import fetch from 'isomorphic-unfetch'
 
 let apolloClient = null
+
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
+  credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
+})
+
+const wsLink = process.browser
+  ? new SubscriptionClient(`ws://localhost:3020/api/ws`, {
+      reconnect: true,
+      connectionParams: {
+        // Connection parameters to pass some validations
+        // on server side during first handshake
+      }
+    })
+  : null
+
+// https://github.com/apollographql/subscriptions-transport-ws/issues/333
+const link = process.browser
+  ? split(
+      //only create the split in the browser
+      // split based on operation type
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink
 
 // Polyfill fetch() on the server (used by apollo-client)
 if (!process.browser) {
@@ -13,10 +45,7 @@ function create(initialState) {
   return new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-    link: new HttpLink({
-      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
-      credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
-    }),
+    link,
     cache: new InMemoryCache().restore(initialState || {})
   })
 }
