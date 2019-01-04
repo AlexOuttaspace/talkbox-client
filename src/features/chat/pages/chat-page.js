cde82888f218bd22e4398de529ff143a3e63bda7
alexander.pomazan <alexander.pomazan@jetruby.com>
2019-01-05 00:06:54 +0300

    feat: add input and teams containers

--- src/features/chat/pages/chat-page.js
+++ src/features/chat/pages/chat-page.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-import { Sidebar } from '../organisms'
+import { Header, Sidebar } from '../organisms'
 import { ChatLayout } from '../templates'
 
 export const ChatPage = () => {
   return (
     <ChatLayout
       sidebarComponent={<Sidebar />}
+      headerComponent={<Header />}
       messagesComponent={<h2>Messages component</h2>}
+      teamsComponent={<h2>Teams component</h2>}
+      inputComponent={<h2>Input component</h2>}
     />
   )
 }
--- src/features/chat/templates/chat-layout.js
+++ src/features/chat/templates/chat-layout.js
@@ -2,34 +2,50 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { Header } from 'src/ui/organisms'
-
 const Root = styled.div`
   height: 100vh;
   display: grid;
-  grid-template-columns: 220px auto;
-  grid-template-rows: 53px auto;
+  grid-template-columns: 80px 220px auto;
+  grid-template-rows: 53px auto 80px;
   grid-auto-columns: 1fr;
+
+  > * {
+    border: 1px solid red;
+  }
+`
+
+const TeamsContainer = styled.div`
+  grid-area: 1 / 1 / 4 / 2;
 `
 
 const SidebarContainer = styled.div`
-  grid-area: 1 / 1 / 3 / 2;
+  grid-area: 1 / 2 / 4 / 3;
 `
 
 const HeaderContainer = styled.div`
-  grid-area: 1 / 2 / 2 / 3;
+  grid-area: 1 / 3 / 2 / 4;
 `
 
 const MessagesContainer = styled.div`
-  grid-area: 2 / 2 / 3 / 3;
+  grid-area: 2 / 3 / 3 / 4;
 `
 
-export const ChatLayout = ({ sidebarComponent, messagesComponent }) => {
+const InputContainer = styled.div`
+  grid-area: 3 / 3 / 4 / 4;
+`
+
+export const ChatLayout = ({
+  headerComponent,
+  sidebarComponent,
+  messagesComponent,
+  teamsComponent,
+  inputComponent
+}) => {
   return (
     <Root>
-      <HeaderContainer>
-        <Header />
-      </HeaderContainer>
+      <TeamsContainer>{teamsComponent}</TeamsContainer>
+      <InputContainer>{inputComponent}</InputContainer>
+      <HeaderContainer>{headerComponent}</HeaderContainer>
       <SidebarContainer>{sidebarComponent}</SidebarContainer>
       <MessagesContainer>{messagesComponent}</MessagesContainer>
     </Root>
@@ -37,6 +53,9 @@ export const ChatLayout = ({ sidebarComponent, messagesComponent }) => {
 }
 
 ChatLayout.propTypes = {
+  headerComponent: PropTypes.element.isRequired,
   sidebarComponent: PropTypes.element.isRequired,
-  messagesComponent: PropTypes.element.isRequired
+  messagesComponent: PropTypes.element.isRequired,
+  teamsComponent: PropTypes.element.isRequired,
+  inputComponent: PropTypes.element.isRequired
 }
